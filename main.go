3354e96642e16ed7f1cf38d3893fdfe663716a1f
fteychene <francois.teychene@gmail.com>
2018-04-23 20:25:17 +0200

    refactor: Split DNS server and Record generation from docker containers

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/pkg/errors"
+
 	docker "docker.io/go-docker"
 	"docker.io/go-docker/api/types"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/jinzhu/copier"
 	"github.com/miekg/dns"
 )
@@ -17,34 +18,6 @@ import (
 type ContainerRegistration map[string][]string
 
 var records = ContainerRegistration{}
-
-func parseQuery(m *dns.Msg) {
-	for _, q := range m.Question {
-		switch q.Qtype {
-		case dns.TypeA:
-			log.Printf("Query for %s\n", q.Name)
-			for _, ip := range records[q.Name] {
-				rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
-				if err == nil {
-					m.Answer = append(m.Answer, rr)
-				}
-			}
-		}
-	}
-}
-
-func handleDNSRequest(w dns.ResponseWriter, r *dns.Msg) {
-	m := new(dns.Msg)
-	m.SetReply(r)
-	m.Compress = false
-
-	switch r.Opcode {
-	case dns.OpcodeQuery:
-		parseQuery(m)
-	}
-
-	w.WriteMsg(m)
-}
 
 func nameToHostname(name string) string {
 	return strings.TrimPrefix(name, "/") + ".container."
@@ -75,34 +48,78 @@ func MergeContainerRegistration(origin, merge ContainerRegistration) ContainerRe
 	return result
 }
 
-func main() {
-	// attach request handler func
-	dns.HandleFunc("container.", handleDNSRequest)
+func GenerateRecordsForContainers(client *docker.Client) (ContainerRegistration, error) {
+	result := make(ContainerRegistration)
+	containers, err := client.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		return nil, errors.Wrap(err, "Error calling container list on docker cli")
+	}
 
+	for _, container := range containers {
+		newRecords := containerToService(container)
+		records = MergeContainerRegistration(result, newRecords)
+	}
+	return result, nil
+}
+
+func parseQuery(m *dns.Msg) {
+	for _, q := range m.Question {
+		switch q.Qtype {
+		case dns.TypeA:
+			log.Printf("Query for %s\n", q.Name)
+			for _, ip := range records[q.Name] {
+				rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
+				if err == nil {
+					m.Answer = append(m.Answer, rr)
+				}
+			}
+		}
+	}
+}
+
+func handleDNSRequest(w dns.ResponseWriter, r *dns.Msg) {
+	m := new(dns.Msg)
+	m.SetReply(r)
+	m.Compress = false
+
+	switch r.Opcode {
+	case dns.OpcodeQuery:
+		parseQuery(m)
+	}
+
+	w.WriteMsg(m)
+}
+
+func StartDNSServer(host string, port int) (*dns.Server, error) {
+	dns.HandleFunc("container.", handleDNSRequest)
+	server := &dns.Server{Addr: host + ":" + strconv.Itoa(port), Net: "udp"}
+	log.Printf("Starting at %d\n", port)
+	err := server.ListenAndServe()
+	if err != nil {
+		return nil, errors.Wrap(err, "Error starting dns server")
+	}
+	return server, nil
+}
+
+func main() {
 	cli, err := docker.NewEnvClient()
 	if err != nil {
 		panic(err)
 	}
 
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	records, err = GenerateRecordsForContainers(cli)
 	if err != nil {
+		log.Fatalf("%e\n", err)
 		panic(err)
 	}
 
-	for _, container := range containers {
-		newRecords := containerToService(container)
-		records = MergeContainerRegistration(records, newRecords)
-	}
-
-	spew.Dump(records)
-
 	// start server
 	port := 53
-	server := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: "udp"}
-	log.Printf("Starting at %d\n", port)
-	err = server.ListenAndServe()
-	defer server.Shutdown()
+
+	dnsServer, err := StartDNSServer("", port)
+	defer dnsServer.Shutdown()
 	if err != nil {
-		log.Fatalf("Failed to start server: %s\n ", err.Error())
+		log.Fatalf("%e\n ", err)
+		panic(err)
 	}
 }
